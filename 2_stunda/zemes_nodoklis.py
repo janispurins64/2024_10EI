d1208b19dc0333b5fafba547e9fa8103a2d38ba6
janispurins64 <54063076+janispurins64@users.noreply.github.com>
2024-09-25 09:36:42 +0300

    Saraksts
    Ieviests saraksts koordinātēm.

--- 2_stunda/zemes_nodoklis.py
+++ 2_stunda/zemes_nodoklis.py
@@ -4,22 +4,59 @@ import math
     Vairākrindu komentārs
     Funkcija malas garuma aprēķinam
 '''
-def malas_garums(ax, ay, bx, by):
-    return math.sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by))
+def malas_garums(a: list, b: list):
+    return math.sqrt((a[0]-b[0])*(a[0]-b[0])+(a[1]-b[1])*(a[1]-b[1]))
 
-# A, B, O koordinātas
-a_x = 0.0
-a_y = 0.0
-b_x = 1.0
-b_y = 4.0
-o_x = 4.0
-o_y = 1.0
+# Funkcija laukuma aprēķinam
+def laukums(a: float, b: float, c: float):
+    p = (a+b+c)/2.0
+    return math.sqrt(p*(p-a)*(p-b)*(p-c))
 
-mala_a = malas_garums(o_x, o_y, b_x, b_y)
-mala_b = malas_garums(o_x, o_y, a_x, a_y)
-mala_o = malas_garums(a_x, a_y, b_x, b_y)
+# A, B, O koordinātas 1.trs.
+a = [0.0, 0.0]
+b = [0.0, 10.0]
+o = [15.0, 5.0]
 
+mala_a = malas_garums(o, b)
+mala_b = malas_garums(o, a)
+mala_o = malas_garums(a, b)
+laukums_1 = laukums(mala_a, mala_b, mala_o)
 
-print(f"Mala a={mala_a}")
-print(f"Mala b={mala_b}")
-print(f"Mala o={mala_o}")
+print(f"Trīsstūta laukums S={laukums_1}")
+# A, B, O koordinātas 2.trs.
+a = [0.0, 10.0]
+b = [30.0, 10.0]
+o = [15.0, 5.0]
+
+mala_a = malas_garums(o, b)
+mala_b = malas_garums(o, a)
+mala_o = malas_garums(a, b)
+laukums_2 = laukums(mala_a, mala_b, mala_o)
+
+print(f"Trīsstūta laukums S={laukums_2}")
+# A, B, O koordinātas 3.trs.
+a = [30.0, 10.0]
+b = [30.0, 0.0]
+o = [15.0, 5.0]
+
+mala_a = malas_garums(o, b)
+mala_b = malas_garums(o, a)
+mala_o = malas_garums(a, b)
+laukums_3 = laukums(mala_a, mala_b, mala_o)
+
+print(f"Trīsstūta laukums S={laukums_3}")
+
+# A, B, O koordinātas 4.trs.
+a = [30.0, 0.0]
+b = [0.0, 0.0]
+o = [15.0, 5.0]
+
+mala_a = malas_garums(o, b)
+mala_b = malas_garums(o, a)
+mala_o = malas_garums(a, b)
+laukums_4 = laukums(mala_a, mala_b, mala_o)
+
+print(f"Trīsstūta laukums S={laukums_4}")
+
+print(f"Lauka laukums S={laukums_1+laukums_2+laukums_3+laukums_4}")
+
