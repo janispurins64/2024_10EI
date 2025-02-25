5959a826fd6d7c63f6c64d09bd5d3468d5fc4f90
janispurins64 <janispurins64@gmail.com>
2025-02-25 14:39:25 +0200

    CSV izdruka
    Izdrukā rindas , pielikts ceļš

--- 26_stunda_CSV/csv_py.py
+++ 26_stunda_CSV/csv_py.py
@@ -1,14 +1,7 @@
 import csv
 import os
-print("---------------------")
-print(os.get_exec_path())
-print("---------------------")
-print(os.getcwdb())
-print("---------------------")
-print(os.getcwd())
-print("---------------------")
 
 with open(os.getcwd()+'\\26_stunda_CSV\\dati_100.csv',newline='') as csv10ei:
     visidati = csv.reader(csv10ei,delimiter=',',quotechar='"')
-    #for row in visidati:
-     #   print(row)
+    for row in visidati:
+       print(row)
