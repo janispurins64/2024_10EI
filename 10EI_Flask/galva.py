f26d34bf3ec82aac284fa4c5591bbff0dd73d264
janispurins64 <54063076+janispurins64@users.noreply.github.com>
2025-05-07 10:30:30 +0300

    Sagatave foram

--- 10EI_Flask/galva.py
+++ 10EI_Flask/galva.py
@@ -40,8 +40,39 @@ def dati():
 @app.route('/dati2',methods=['GET','POST']) #Trīsstūra malas
 def dati2():
    a = request.args.get('mala_a') # Tiek izmantots input atribūts name
-   print(f"Mala a= {a}") 
+   b = request.args.get('mala_b') # Tiek izmantots input atribūts name   
+   c = request.args.get('mala_c') # Tiek izmantots input atribūts name
+   print(f"Mala a= {a}")
+   print(f"Mala b= {b}") 
+   print(f"Mala c= {c}")
+   print(trisTips(a, b, c))
    return render_template("dati.html") 
+
+#----------------------------- Funkcijas
+def irTrissturis(a, b, c):
+    if a<(b+c) and b<(c+a) and c<(a+b):
+        return True
+    else:
+        return False
+
+def trisTips(a, b, c):
+    malas = [a, b, c]
+    malas.sort()
+    if c*c>(a*a+b*b):
+        return "Platleņķa"
+    elif c*c<(a*a+b*b):
+        return "Šaurleņķa"
+    else:
+        return "Taisnleņķa"        
+def trisATips(a, b, c):
+    malas = [a, b, c]
+    malas.sort()
+    if a==c:
+        return "Vienādmalu"
+    elif a==b or b==c:
+        return "Vienādsānu"
+    else:
+        return "Dažādmalu"     
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
